6d0a83478ccaf5a3d17bf472597c98022ea51a60
xiangnan321 <482724128@qq.com>
2021-03-04 18:03:50 +0800

    2222

--- unpackage/dist/dev/mp-alipay/common/vendor.js
+++ unpackage/dist/dev/mp-alipay/common/vendor.js
@@ -9655,17 +9655,6 @@ module.exports = g;
 
 
 
-/***/ }),
-
-/***/ 42:
-/*!*******************************************!*\
-  !*** D:/明腾/新建文件夹/Hotel-key/static/fk.png ***!
-  \*******************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-module.exports = "/static/fk.png";
-
 /***/ })
 
 }]);
